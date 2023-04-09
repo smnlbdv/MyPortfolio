2a5c1c1a869e4b29ab5d0c25a5dd9efce7a295ac
Semen Lebedew <mamontedinorog@yandex.by>
2023-04-10 00:58:35 +0300

    style+media+preloader

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,6 +24,7 @@ function clickInfo () {
   } 
 }
 
+
 function onEntry(entry) {
   entry.forEach(change => {
     if (change.isIntersecting) {
@@ -32,29 +33,53 @@ function onEntry(entry) {
   });
 }
 
-function addActive(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-     change.target.classList.add('active');
-    }
-  });
-}
+
 
 let options = {
   threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let addLinksActive = new IntersectionObserver(addActive, options);
-let elements = document.querySelectorAll('.section__about-me');
+
+let observerSection = new IntersectionObserver(onEntry, options);
+let elements = document.querySelector('.section__about-me');
 let elements1 = document.querySelectorAll('.section__work_item');
+
+observerSection.observe(elements);
+
+elements1.forEach(item => {
+  observerSection.observe(item)
+});
+
+
 let linkElement = document.querySelector('.link1')
+let linkElement2 = document.querySelector('.link2')
 
-for (let elm of elements) {
-  observer.observe(elm);
-  addLinksActive.observe(linkElement)
+let observerLinkFirst = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if(entry.isIntersecting == true) {
+      linkElement.classList.add('active')
+    } else {
+      linkElement.classList.remove('active')
+    }
+  });
+});
+
+let observerLinkTwo = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if(entry.isIntersecting == true) {
+      linkElement2.classList.add('active')
+    } else {
+      linkElement2.classList.remove('active')
+    }
+  });
+});
+
+let linkAboutMe = document.querySelector('.section__about-me');
+let linkWork = document.querySelector('.section__work');
+
+observerLinkFirst.observe(linkAboutMe);
+observerLinkTwo.observe(linkWork);
+
+
+window.onload = function () {
+  let preloader = document.getElementById('preloader');
+  preloader.style.display = 'none'
 }
-
-for (let elm of elements1) {
-  observer.observe(elm);
-}
-
-
