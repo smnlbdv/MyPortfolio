55d897490cf98f82e90661d53b22b7e1422d91f1
Semen Lebedew <mamontedinorog@yandex.by>
2023-05-11 16:19:06 +0300

    new style + swiper options

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
 //**Creating a swiper**//
 const swiper = new Swiper('.swiper', {
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
 });
 
 //**Variable declaration**//
